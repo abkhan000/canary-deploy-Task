5c2d99c6770a217bd42dde9352176dec56679afa
abrarkhan <123900280+abkhan000@users.noreply.github.com>
2024-09-18 14:57:31 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-northeast-1"
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "my-eks-cluster"
+  default     = "k8s-cluster"
 }
